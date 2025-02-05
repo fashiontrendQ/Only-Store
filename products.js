8d9260093598c035b39acb18533ec6002288604d
fashiontrendQ <rmalik963y@gmail.com>
2025-02-05 11:45:19 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,66 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Sweet Doughnut",
+        "price": 100,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0013.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Sweet Doughnut",
+        "price": 200,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0006.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Sweet Doughnut",
+        "price": 100,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0007.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Sweet Doughnut",
+        "price": 155,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0008.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Sweet Doughnut",
+        "price": 110,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0009.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Sweet Doughnut",
+        "price": 100,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0010.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Sweet Doughnut",
+        "price": 100,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0011.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Sweet Doughnut",
+        "price": 100,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0012.jpg"
+    },
+    {
+        "id": 9,
+        "name": "Sweet Doughnut",
+        "price": 125,
+        "description": "Very sweet made with coconut",
+        "image": "images/IMG-20250204-WA0014.jpg"
+    },
+];
+export default products;
